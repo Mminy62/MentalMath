2f693dad8d9e63cedfa1f731062d80b72049aa9f
Minyoung Lee <mminy62@gmail.com>
2024-08-29 02:19:27 +0900

    Init: 프로젝트 생성

---
+++ MathMastery/MathMastery/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MathMastery
+//
+//  Created by 이민영 on 8/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathMastery/MathMastery/MathMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathMasteryApp.swift
+//  MathMastery
+//
+//  Created by 이민영 on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MathMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
