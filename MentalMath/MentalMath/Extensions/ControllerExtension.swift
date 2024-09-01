bcbbe78c0321e7323c4979863e98b22639dacfe6
Minyoung Lee <mminy62@gmail.com>
2024-09-02 02:19:53 +0900

    Feat: 뒤로가기 swipe 기능 추가

---
+++ MentalMath/MentalMath/Extensions/ControllerExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ControllerExtension.swift
+//  MentalMath
+//
+//  Created by 이민영 on 9/1/24.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
